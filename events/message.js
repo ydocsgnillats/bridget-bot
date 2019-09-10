fbf493d3b6cf73d4c175b1b11a3d842b62bb6304
Cody Stallings <cmstal0507@ung.edu>
2019-09-10 15:35:35 -0400

    added some stuff from heroku

--- events/message.js
+++ events/message.js
@@ -1,8 +1,12 @@
 const kick = require('../commands/kick')
+const note = require('../commands/note')
 
 module.exports = (client, message) => {
 	if (message.content.startsWith('!kick')) {
 		return kick(message)
 	}
+
+	if (message.content.startsWith('bridget!')) {
+		return note(message)
+	}
 }
-    
