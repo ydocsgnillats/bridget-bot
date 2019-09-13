64353121a23b4a82832f5492926f5a32839d9ffe
Cody Stallings <cmstal0507@ung.edu>
2019-09-13 12:35:40 -0400

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,12 +1,38 @@
 const kick = require('../commands/kick')
 const note = require('../commands/note')
+const log = require('../commands/log')
+const pin = require('../commands/pin')
 
 module.exports = (client, message) => {
+
 	if (message.content.startsWith('!kick')) {
 		return kick(message)
 	}
 
-	if (message.content.startsWith('bridget!')) {
+	if (message.content.startsWith('Bridget!')) {
+		log(message)
 		return note(message)
+	}
+	if (message.content.startsWith('bridget!')) {
+		log(message)
+		return note(message)
+	}
+	if (message.content.startsWith('Bridget')) {
+		log(message)
+		return note(message)
+	}
+	if (message.content.startsWith('bridget write that down')) {
+		log(message)
+		return note(message)
+	}
+	if (message.content.startsWith('Bridget write that down')) {
+		log(message)
+		return note(message)
+	}
+	if (message.content.startsWith('pin that')) {
+		return pin(message)
+	}
+	if (message.content.startsWith('Pin that')) {
+		return pin(message)
 	}
 }
