6b4d37cf586b21bd3a5d576e55a96a0844a12f86
Cody Stallings <ydocsgnillats@gmail.com>
2019-09-25 14:44:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const activities_list = [
-    "Ram Ranch", 
-    "Gamers Rise Up",
-    "your stupid ideas over in her head", 
-    "goat simulator",
+    	"Ram Ranch", 
+    	"Gamers Rise Up",
+    	"your stupid ideas over in her head", 
+    	"goat simulator",
 	"the BYAND",
 	"planetary annihalation",
 	"HE-MAN HEYEAYEA SONG FOR 10 HOURS",
 	"OWO what's this???",
 	"My Little Pony: Friendship is Magic",
-	"the role of the secretary we never had"
+	"the role of the secretary we never had",
+	"Halo MCCPC",
+	"Destiny",
+	"KFC dating simulator",
+	"Warcraft: Reforged",
+	"Battletoads 2",
+	"Nightmares of a forgotten past",
+	"Nightcrawlers"
+	
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 fs.readdir('./events/', (err, files) => {
@@ -26,6 +34,7 @@ fs.readdir('./events/', (err, files) => {
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
   setInterval(() => {
+	client.user.setActivity("waking up");
 	const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
 	client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
   }, 300000); // Runs this every 20 minutes.
