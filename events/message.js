9adbf8678e11884311bce4c1bce9bac8837a9e55
Cody Stallings <ydocsgnillats@gmail.com>
2019-09-25 13:21:55 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -17,8 +17,10 @@ module.exports = (client, message) => {
 	if (message.content.includes('Pin that')) {
 		return pin(message)
 	}
-	if (message.content.includes(' 69 ')) {
-		return nice(message)
+	if (message.content.includes('69')) {
+		for(var i = 0; i <= 1; i++){
+			return nice(message)
+		}
 	}
 	if (message.content.includes('thanks bridget')) {
 		return welcome(message)
