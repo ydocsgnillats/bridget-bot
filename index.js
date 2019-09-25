c4e3430f4604e8889cf45b50b7e608138722b431
Cody Stallings <ydocsgnillats@gmail.com>
2019-09-25 14:45:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ fs.readdir('./events/', (err, files) => {
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
+  client.user.setActivity("waking up");
   setInterval(() => {
-	client.user.setActivity("waking up");
 	const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
 	client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
   }, 300000); // Runs this every 20 minutes.
