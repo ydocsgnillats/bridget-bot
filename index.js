f2be27e67b9ab8f3b842e654f35c132ffeab59fd
Cody Stallings <cmstal0507@ung.edu>
2019-09-26 12:04:06 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ fs.readdir('./events/', (err, files) => {
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setActivity("Startup " + process.env.START);
-  process.env.START+1;
+  client.user.setActivity("Startup Dev");
   setInterval(() => {
 	const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
 	client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
