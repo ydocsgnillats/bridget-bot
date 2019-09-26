796ed1ef4828853f6f59c6f9867d234ff9a353d7
Cody Stallings <cmstal0507@ung.edu>
2019-09-26 11:27:40 -0400

    bridget stop calling yourself

--- events/message.js
+++ events/message.js
@@ -3,45 +3,30 @@ const pin = require('../commands/pin')
 const welcome = require('../commands/welcome')
 
 module.exports = (client, message) => {
+		if(message.author.bot) return;
 
 		if (message.content.startsWith('Bridget!')) {
-			if (message.author == client.user){
-				return note(message)
-			}
+			return note(message)
 		}
 		if (message.content.startsWith('bridget!')) {
-			if (message.author == client.user){
-				return note(message)
-			}
+			return note(message)
 		}
 		if (message.content.includes('pin that')) {
-			if (message.author == client.user){
-				return pin(message)
-			}
+			return pin(message)
 		}
 		if (message.content.includes('Pin that')) {
-			if (message.author == client.user){
-				return pin(message)
-			}
+			return pin(message)
 		}
 		if (message.content.includes('thanks bridget')) {
-			if (message.author == client.user){
-				return welcome(message)
-			}
+			return welcome(message)
 		}
 		if (message.content.includes('Thanks Bridget')) {
-			if (message.author == client.user){
-				return welcome(message)
-			}
+			return welcome(message)
 		}
 		if (message.content.includes('Thanks bridget')) {
-			if (message.author == client.user){
-				return welcome(message)
-			}
+			return welcome(message)
 		}
 		if (message.content.includes('thanks Bridget')) {
-			if (message.author == client.user){
-				return welcome(message)
-			}
+			return welcome(message)
 		}
 	}
