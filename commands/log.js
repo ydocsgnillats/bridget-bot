269c923090a7b42032f0aadb8d303629da07afd5
Cody Stallings <cmstal0507@ung.edu>
2019-10-04 15:22:10 -0400

    Add files via upload

---
+++ commands/log.js
@@ -0,0 +1,9 @@
+module.exports = message => {
+	var msgCollect = message.channel.messages;
+	var msgArray = Array.from(msgCollect.values());
+	var prevMsg = msgArray[msgArray.length -2];
+	var fs = require("fs");
+    var stream;
+    stream = fs.createWriteStream("../ideas/idea.txt");
+    stream.write(prevMsg);
+}
