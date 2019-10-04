2d98a8a2772e80278fca6e5ed938b2a20dbab24a
Cody Stallings <ydocsgnillats@gmail.com>
2019-10-04 15:32:10 -0400

    Update note.js

--- commands/note.js
+++ commands/note.js
@@ -1,12 +1,4 @@
-var fs = require('fs');
-var stream;
-stream = fs.createWriteStream('./ideas/idea.txt');
-
 module.exports = message => {
-  var msgCollect = message.channel.messages;
-  var msgArray = Array.from(msgCollect.values());
-  var prevMsg = msgArray[msgArray.length -2];
+
   message.reply('Writing that down.')	
-  // file system module to perform file operations
-  stream.write(prevMsg)
   }
