24e27dc69681b969c567099df6bacf215c99e31a
Cody Stallings <cmstal0507@ung.edu>
2019-10-04 15:22:43 -0400

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -2,14 +2,17 @@ const note = require('../commands/note')
 const pin = require('../commands/pin')
 const welcome = require('../commands/welcome')
 const say = require('../commands/say')
+const log = require('../commands/log')
 
 module.exports = (client, message) => {
 		if(message.author.bot) return;
 
 		if (message.content.startsWith('Bridget!')) {
+			log(message)
 			return note(message)
 		}
 		if (message.content.startsWith('bridget!')) {
+			log(message)
 			return note(message)
 		}
 		if (message.content.includes('pin that')) {
