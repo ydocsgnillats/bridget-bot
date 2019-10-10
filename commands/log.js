219c460fa3c83db0543492c1c5ef1540998ee70b
Cody Stallings <ydocsgnillats@gmail.com>
2019-10-10 12:06:04 -0400

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -1,8 +1,8 @@
 module.exports = message => {
-	var msgCollect = message.channel.messages;
-	var msgArray = Array.from(msgCollect.values());
-	var prevMsg = msgArray[msgArray.length -2];
-	var fs = require("fs");
+    var msgCollect = message.channel.messages;
+    var msgArray = Array.from(msgCollect.values());
+    var prevMsg = msgArray[msgArray.length -2];
+    var fs = require("fs");
     var stream;
     stream = fs.createWriteStream("../ideas/idea.txt");
     stream.write(prevMsg);
