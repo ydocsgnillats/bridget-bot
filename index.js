88dbb88158ecb5c62b1d00cd24de2a10ccb96d6a
Cody Stallings <cmstal0507@ung.edu>
2019-10-15 15:08:00 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -75,11 +75,14 @@ client.on('message', async message => {
 		message.channel.fetchMessages()
 			  .then(messages => {
 				if(message.author.bot){
-					message.channel.delete
+					message.channel.delete("botmessages")
 					messagesDeleted = messages.array().length; // number of messages deleted
 					// Logging the number of messages deleted on both the channel and console.
 					message.channel.sendMessage("Bridget messages deleted: "+messagesDeleted);
 					console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+				}
+				else{
+					return message.channel.sendMessage("failed to clear messages");
 				}
 			  })
 			  .catch(err => {
