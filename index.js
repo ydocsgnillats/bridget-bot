80c6d666974dc06fe2934b79ed1a2020f91f77ed
Cody Stallings <cmstal0507@ung.edu>
2019-10-15 15:11:12 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ client.on('message', async message => {
 	if(cmd === 'bclear!'){
 		message.channel.fetchMessages()
 			  .then(messages => {
-				if(message.author.bot){
-					message.channel.delete("botmessages")
+				if(messages.author.bot){
+					message.channel.bulkDelete(10)
 					messagesDeleted = messages.array().length; // number of messages deleted
 					// Logging the number of messages deleted on both the channel and console.
 					message.channel.sendMessage("Bridget messages deleted: "+messagesDeleted);
