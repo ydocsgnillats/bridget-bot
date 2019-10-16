63b706c0ce2efef64b43cc2f7727572beaba3de5
Cody Stallings <ydocsgnillats@gmail.com>
2019-10-16 14:18:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const activities_list = [
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`)
   let date = new Date();
-  client.user.setActivity("Initialization: " + (date.getSeconds()), {type: "PLAYING"});
+  client.user.setActivity("Initialization: " + (date.getSeconds()), {type: "STREAMING"});
   setInterval(() => {
 	const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
 	client.user.setActivity(activities_list[index], {type: "STREAMING"}); // sets bot's activities to one of the phrases in the arraylist.
