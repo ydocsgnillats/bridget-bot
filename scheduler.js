d62f4297032bc28129060daffbb61ca282d8f51e
Cody Stallings <ydocsgnillats@gmail.com>
2020-02-03 09:40:35 -0500

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,5 +1,6 @@
+module.exports = message => {
 	var msgCollect = message.channel.messages;
 	var msgArray = Array.from(msgCollect.values());
 	var prevMsg = msgArray[msgArray.length -2];
-
-    return message.channel.send("When should I remind you about " + "*" + prevMsg + "?*")
+	return message.channel.send("When should I remind you about " + "*" + prevMsg + "?*")
+}
