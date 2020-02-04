177117740a5465050fa66d503323914d64f2def9
Cody Stallings <cmstal0507@ung.edu>
2020-02-04 10:01:30 -0500

    updated scheduler

--- scheduler.js
+++ scheduler.js
@@ -1,10 +1,10 @@
 module.exports = message => {
 	var msgarray = message.content.split(" ").slice(1).join(" ");
-	message.channel.fetchMessages({ 
-		limit: 3 // Fetch last 50 messages.
-	}).then((msgCollection) => { // Resolve promise
-	msgCollection.forEach((msg) => { // forEach on message collection
-		msg.delete(); // Delete each message
-	)};
+	//message.channel.fetchMessages({ 
+	//	limit: 3 // Fetch last 3 messages.
+	//}).then((msgCollection) => { // Resolve promise
+	//msgCollection.forEach((msg) => { // forEach on message collection
+	//	msg.delete())// Delete each message
+	
 	return message.channel.send("When should I remind you about " + "*" + msgarray + "?*").catch(e => console.log(e));
 }
