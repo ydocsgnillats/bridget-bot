4b669982618af714deb5acaea8d5e28d3bc9c5a0
Cody Stallings <cmstal0507@ung.edu>
2020-02-04 14:19:26 -0500

    Add files via upload

--- ideaRead.js
+++ ideaRead.js
@@ -3,6 +3,6 @@ module.exports = message => {
     const fs = require('fs')
     ideas[message.author].idea += message;
     var jsonText = fs.readFileSync("./ideas.json")
-    var text = JSON.stringify(ideas.json);
+    var text = JSON.stringify(ideas);
     return message.channel.send("Current notes: " + text)
 }
