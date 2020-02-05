4d920ad97fa8499b2e7f758b7d89ec41bf38aaf4
Cody Stallings <cmstal0507@ung.edu>
2020-02-05 12:35:32 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
 
+const { Client } = require('pg');
+
+const dbclient = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+dbclient.connect();
+
+dbclient.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  dbclient.end();
+});
+
 const scheduler = require('./scheduler.js')
 const note = require('./note.js')
 const read = require('./ideaRead.js')
