a72f135d12bcb9dc9c624ab5f6d156619e51a554
Cody Stallings <cmstal0507@ung.edu>
2020-02-05 10:56:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -68,10 +68,22 @@ client.on('message', async message => {
 	if(message.content.includes('clear!')){
 		return clear(message)
 	}
-	if(message.content.includes('help!')){
-		return help(message)
+	if(message.content.includes('help!')){    
+		let sEmbed = new Discord.RichEmbed()
+		.setColor('#ffcba4')
+		.setTitle("Bridget:")
+		.setDescription("A secretary Bot")
+		.addField("**bridget!**", "writes things down", true)
+		.addField("**ideas!**", "recalls ideas by user", true)
+		.addField("**pin!**", "pins the last message", true)
+		.addField("**thanks**", "you're welcome", true)
+		.addField("**pay respect**", "get an F in the chat", true)
+		.addField("**roll!**", "random roll between 1 and 100", true)
+		.addField("**schedule!**", "schedule reminders", true)
+		.addField("**help!**", "sends this message", true)
+		.setFooter('BridgetBot2020', client.user.displayAvatarURL);
+		return message.channel.send({embed: sEmbed});
 	}
 })
 
-exports.client = client
 client.login(process.env.TOKEN)
