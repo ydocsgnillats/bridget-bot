3407f894ca80f20c58b4160ad269312efec840f4
Cody Stallings <ydocsgnillats@gmail.com>
2020-02-07 12:10:24 -0500

    Delete ideaRead.js

--- ideaRead.js
+++
@@ -1,8 +0,0 @@
-module.exports = message => {
-    const ideas = require('./ideas.json')
-    const fs = require('fs')
-    //ideas[message.author].idea += message;
-    var jsonText = fs.readFileSync("./ideas.json")
-    var text = JSON.stringify(ideas);
-    return message.channel.send("Current notes: " + text)
-}
