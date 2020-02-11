5a8c7760a90ba7fda2d2c7543f2fcfe9a2552b82
Cody Stallings <cmstal0507@ung.edu>
2020-02-11 11:36:13 -0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,26 @@
+import datastore from 'nedb-promise'
+
+async function doDatabaseStuff() {
+  let DB = datastore({
+     // these options are passed through to nedb.Datastore
+
+     filename: './db.json',
+
+     autoload: true // so that we don't have to call loadDatabase()
+  })
+
+  await DB.insert([{
+    num: 1, alpha: 'a'
+  }, {
+    num: 2, alpha: 'b'
+  }])
+
+  let document = await DB.findOne({ num: 1 })
+
+  // use NeDB cursors:
+  let documents = await DB.cfind({})
+    .projection({ num: 1, _id: 0 })
+    .exec()
+}
+
+return doDatabaseStuff()
