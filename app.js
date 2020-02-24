4825d7e89b3637748f3af90adb64ae571597483b
Cody Stallings <cmstal0507@ung.edu>
2020-02-24 12:00:13 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ client.on('ready', async () => {
 	const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list.
 	client.user.setActivity(activities_list[index], {type: "STREAMING"}); // sets bot's activities to stream one of the phrases in the arraylist.
   }, 300000); // Runs this every 5 minutes.
-  Ideabase.sync({ force: true })
 })
 
 client.on('message', async message => {
@@ -124,6 +123,11 @@ client.on('message', async message => {
 		.setFooter('BridgetBot2020', client.user.displayAvatarURL);
 		return message.channel.send({embed: sEmbed});
 	}
+})
+
+client.off('shutdown', async () => {
+	console.log(`${client.user.tag} is shutting down...`)
+	Ideabase.sync()
 })
 
 client.login(process.env.TOKEN)
