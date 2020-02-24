b7431627c5c4d2753c53263111b8d44af0273edb
Cody Stallings <cmstal0507@ung.edu>
2020-02-24 10:31:30 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js')
 const Sequelize = require('sequelize')
 const client = new Discord.Client()
 const fs = require('fs')
-const Ideabase = require('dbsetup.js')
+const Ideabase = require('./models/dbsetup')
 
 const scheduler = require('./commands/scheduler.js')
 const pin = require('./commands/pin.js')
