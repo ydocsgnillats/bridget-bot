121569c504be9054c8710e42da6d87ca3f69150a
Cody Stallings <cmstal0507@ung.edu>
2020-02-24 10:07:58 -0500

    Add files via upload

---
+++ models/dbsetup.js
@@ -0,0 +1,32 @@
+if (!global.hasOwnProperty('db')) {
+    var Sequelize = require('sequelize')
+      , sequelize = null
+  
+    if (process.env.DATABASE_URL) {
+      // the application is executed on Heroku ... use the postgres database
+      sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect:  'postgres',
+        protocol: 'postgres',
+        port:     match[4],
+        host:     match[3],
+        logging:  true //false
+      })
+    } else {
+      // the application is executed on the local machine ... use mysql
+      sequelize = new Sequelize('bridget-sec-bot-db', 'root', null)
+    }
+  
+    global.db = {
+      Sequelize: Sequelize,
+      sequelize: sequelize,
+      User:      sequelize.import(__dirname + '/user') 
+      // add your other models here
+    }
+  
+    /*
+      Associations can be defined here. E.g. like this:
+      global.db.User.hasMany(global.db.SomethingElse)
+    */
+  }
+  
+  module.exports = global.db
