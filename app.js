fdf94b99e80d6c0684b34db6e6e3af834fe962cb
Cody Stallings <cmstal0507@ung.edu>
2020-02-24 10:47:44 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,18 +4,51 @@ const Discord = require('discord.js')
 const Sequelize = require('sequelize')
 const client = new Discord.Client()
 const fs = require('fs')
-const Ideabase = require('./models/index')
 
 const scheduler = require('./commands/scheduler.js')
 const pin = require('./commands/pin.js')
 const roll = require('./commands/roll.js')
 const activities = require('./commands/activities.js')
 
-//,
-//	dialect: 'sqlite',
-//	logging: false,
-//	storage: 'database.sqlite',
-//})
+if (!global.hasOwnProperty('db')) {
+    var Sequelize = require('sequelize')
+      , sequelize = null
+  
+    if (process.env.DATABASE_URL) {
+      // the application is executed on Heroku ... use the postgres database
+      const sequelize = new Sequelize('database', 'user', 'password', {
+    	host: 'process.env.DATABASE_URL',
+        dialect:  'postgres',
+        protocol: 'postgres',
+        logging:  true //false
+      })
+    } else {
+      // the application is executed on the local machine ... use mysql
+      sequelize = new Sequelize('bridget-sec-bot-db', 'root', null)
+    }
+  
+    global.db = {
+      Sequelize: Sequelize,
+      sequelize: sequelize,
+      //User:      sequelize.import(__dirname + '/user') 
+      // add your other models here
+    }
+
+    
+const Ideabase = sequelize.define('ideas', {
+	username: {
+		type: Sequelize.STRING,
+	},
+	name: Sequelize.STRING,
+	note: Sequelize.TEXT,
+	guild: Sequelize.STRING,
+	date: Sequelize.DATE,
+	idea_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+})  
 
 var activities_list = activities.activitylist()
 
