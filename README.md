798cdae680ce067b2c9b9dedf91f08b5f5af1ad7
Cody Stallings <cmstal0507@ung.edu>
2020-04-05 19:22:08 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
-# bridget-bot
+# Bridget-Bot
 Discord Secretary Bot
 
+###Requirements:
+  >.env file with 'BOT_TOKEN=YOUR_BOT_TOKEN'
+  >discord.js
+  >node.js
+  >npm
+  >pg
 
-Instructions:
+###Features:
 
-Requirements:
-  .env file with 'BOT_TOKEN=YOUR_BOT_TOKEN'
-  discord.js
-  node.js
-  npm
+  >Update the 'activities.js' file to change the statuses/activities that bridget cycles through!
+  
+  >Additional words Bridget listens for: (thanks, pay respect, in the chat)
+  
+  
 
-Commands:
-  'bridget!', 'Bridget!' - Take note of the previous message.
-  'pin that', 'Pin that' - Pins the previous message in the channel.
+| Command    | Description           |
+| :-------------: |:-------------:| 
+| test! | Replies with her name in all caps |
+| bridget! or Bridget! | Take note of the previous message |
+| ideas! | Returns notes taken, by user|
+| pin! or Pin! | Pins the previous message in the channel |
+| undo! | Unpins the previous message |
+| roll! | Rolls a number between 1 and 100 |
+| schedule! | Schedules an event, event name is whatever follows 'schedule!' |
+| clear! | Clears her own recent messages from the channel |
+| help! | Returns a list of possible commands to the discord channel itself |
+
+
+
---
+++ main.js
@@ -0,0 +1,96 @@
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const activities = require('./commands/activities.js')
+const Sequelize = require('sequelize')
+const activities_list = activities.activitylist()
+const response = require('./interface.js')
+
+//setting up the app website
+let express = require('express')
+let app = express()
+app.use(express.static('web'))
+app.get('/', function(req, res){
+  res.sendFile('web/index.html', { root : __dirname})
+})
+app.listen(process.env.PORT || 9000)
+
+//setting up postgres databases using sequelize 
+const databaseName = process.env.DATABASE_NAME
+const databaseHost = process.env.DATABASE_HOST
+const databaseUser = process.env.DATABASE_USER
+const databasePort = process.env.DATABASE_PORT
+const databasePassword = process.env.DATABASE_PASSWORD
+
+const sequelize = new Sequelize(databaseName, databaseUser, databasePassword, {
+	dialect: 'postgres',
+	host: databaseHost,
+	port: databasePort,
+	logging: false,
+})
+
+const Ideabase = sequelize.define('ideas', {
+	username: {
+		type: Sequelize.STRING,
+	},
+	name: Sequelize.STRING,
+	note: Sequelize.TEXT,
+	guild: Sequelize.STRING,
+	date: Sequelize.DATE,
+	idea_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+	kill_count: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		defaultValue: 0,
+	},
+})  
+
+const Schedulebase = sequelize.define('schedule', {
+	username: {
+		type: Sequelize.STRING,
+	},
+	event: Sequelize.TEXT,
+	guild: Sequelize.STRING,
+	date: Sequelize.DATE,
+})  
+
+const Motionbase = sequelize.define('motion', {
+	username: {
+		type: Sequelize.STRING,
+	},
+	motion: Sequelize.TEXT,
+	date: Sequelize.DATE,
+	guild: Sequelize.STRING
+})  
+
+// setting up the bot, syncing databases, setting activity
+const date = require('date-and-time')
+const now = new Date()
+date.format(now, 'YYYY/MM/DD HH:mm:ss')
+
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+  let date = new Date()
+  client.user.setActivity("Initialization: " + (date.getSeconds()), {type: "PLAYING"}) 
+  setInterval(() => {
+	const index = Math.floor(Math.random() * (activities_list.length - 1) + 1) // generates a random number between 1 and the length of the activities array list.
+	client.user.setActivity(activities_list[index], {type: "STREAMING"}) // sets bot's activities to stream one of the phrases in the arraylist.
+  }, 300000) // Runs this every 5 minutes.
+  await sequelize.sync()
+})
+
+// listens for a message in the discord server
+client.on('message', async message => {
+    return response.response(message)
+})
+
+client.off('shutdown', async () => {
+	console.log(`${client.user.tag} is shutting down...`)
+})
+
+client.login(process.env.TOKEN)
