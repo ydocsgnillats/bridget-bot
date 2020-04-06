a2a9746d58decdb244815f3082f4500a8b623dcd
Cody Stallings <cmstal0507@ung.edu>
2020-04-06 14:27:37 -0400

    Add files via upload

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,10 @@
-const main = require('main.js')
-const client = main.client
+const main = require('./main.js')
+const avatar = main.avatar
 
 module.exports = {
 	name: 'test',
 	description: 'test!',
 	execute(message, args) {
-        message.channel.send("**B R I D G E T**" + "\n" + client.user.displayAvatarURL) 
+        message.channel.send("**B R I D G E T**" + "\n" + avatar) 
     }
 }
