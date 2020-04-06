885b28585126092d1f69db93a8c9c0009b761333
Cody Stallings <cmstal0507@ung.edu>
2020-04-06 08:29:34 -0400

    Add files via upload

--- interface.js
+++ interface.js
@@ -1,23 +1,20 @@
-require('./main.js')
-require('dotenv').config()
-
 module.exports = {response:response}
 
-function pin(message){
-    var msgCollect = message.channel.messages
-    var msgArray = Array.from(msgCollect.values())
-    var prevMsg = msgArray[msgArray.length -2]
-    prevMsg.pin()
-    return;
-}
+// function pin(message){
+//     var msgCollect = message.channel.messages
+//     var msgArray = Array.from(msgCollect.values())
+//     var prevMsg = msgArray[msgArray.length -2]
+//     prevMsg.pin()
+//     return;
+// }
 
-function unPin(message){
-    var channel = message.channel
-    var msg = channel.fetchPinnedMessages()
-    var msgArray = Array.from(msg);
-    msgArray.unpin()
-    return;
-}
+// function unPin(message){
+//     var channel = message.channel
+//     var msg = channel.fetchPinnedMessages()
+//     var msgArray = Array.from(msg);
+//     msgArray.unpin()
+//     return;
+// }
 
 // function schedule(message){
 //     var msgarray = message.content.split(" ").slice(1).join(" ")
