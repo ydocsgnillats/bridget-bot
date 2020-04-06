ddae310cb76ad37ecf3fc0fd11459307b1ed13cb
Cody Stallings <cmstal0507@ung.edu>
2020-04-06 13:40:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -113,6 +113,24 @@ client.on('ready', async () => {
 client.on('message', async message => {
 	const args = message.content.slice(prefix.length).split(/ +/)
 	const command = args.shift().toLowerCase()
+	if(message.content.includes('help!')){
+		let sEmbed = new Discord.RichEmbed()
+		.setColor('#ffcba4')
+		.setTitle("Bridget:")
+		.setDescription("A secretary Bot")
+		.addField("**bridget!**", "writes things down", true)
+		.addField("**ideas!**", "recalls ideas by user", true)
+		.addField("**clear!**", "clears user's ideas", true)
+		.addField("**pin!**", "pins the last message", true)
+		.addField("**thanks**", "you're welcome", true)
+		.addField("**pay respect**", "get an F in the chat", true)
+		.addField("**roll!**", "random roll between 1 and 100", true)
+		.addField("**schedule!**", "schedule reminders", true)
+		.addField("**kill!**", "kills a user", true)
+		.addField("**help!**", "sends this message", true)
+		.setFooter('BridgetBot2020', client.user.displayAvatarURL)
+		return message.channel.send({embed: sEmbed})
+	}
 	if(message.content.includes('pay respects')){
 		message.channel.send("F.")
 	}
