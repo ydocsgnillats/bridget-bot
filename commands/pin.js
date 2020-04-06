ff7104f5e09d7352f92c3a589520e01a4bf48da7
Cody Stallings <cmstal0507@ung.edu>
2020-04-06 15:52:48 -0400

    Add files via upload

--- commands/pin.js
+++ commands/pin.js
@@ -2,10 +2,10 @@ module.exports = {
         name: 'pin',
 	description: 'Pin!',
 	execute(message, args) {
-        var msgCollect = message.channel.messages
-        var msgArray = Array.from(msgCollect.values())
-        var prevMsg = msgArray[msgArray.length -2]
-        prevMsg.pin()
-        return
+                var msgCollect = message.channel.messages
+                var msgArray = Array.from(msgCollect.values())
+                var prevMsg = msgArray[msgArray.length -2]
+                prevMsg.pin()
+                return
 	}
 }
