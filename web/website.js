40a93176a6795b2a6586f89001d6673dcfb6cb7e
Cody Stallings <cmstal0507@ung.edu>
2020-04-09 18:05:35 -0400

    Add files via upload

--- web/website.js
+++ web/website.js
@@ -1,8 +1,13 @@
-//setting up the app website
 let express = require('express')
 let app = express()
 app.use(express.static('web'))
 app.get('/', function(req, res){
   res.sendFile('./index.html', { root : __dirname})
 })
-app.listen(process.env.PORT || 9000)
+app.listen(9000)
+
+//setting up the app website
+var http = require('http')
+http.createServer(function (req, res){
+  app.sendFile('./index.html', { root : __dirname})
+}).listen(8080)
